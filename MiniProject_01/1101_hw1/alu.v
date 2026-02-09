7f9fc409d66ed170c319984a2def7d892b7bf378
ALI BHANJI <bhanja7@mcmaster.ca>
2026-02-08 21:19:56 -0500

    first 3 operations completed

--- MiniProject_01/1101_hw1/alu.v
+++ MiniProject_01/1101_hw1/alu.v
@@ -17,7 +17,8 @@ reg  [11:0] o_data_w, o_data_r;
 reg         o_valid_w, o_valid_r;
 reg         o_overflow_w, o_overflow_r;
 // ---- Add your own wires and registers here if needed ---- //
-
+reg signed [23:0] overflow_result; // wire
+ref signed [11:0] accumulator; // register
 
 
 
@@ -35,11 +36,71 @@ assign o_overflow = o_overflow_r;
 // ---------------------------------------------------------------------------
 // Combinational Blocks
 // ---------------------------------------------------------------------------
-// ---- Write your conbinational block design here ---- //
+// ---- Write your combinational block design here ---- //
 always@(*) begin
     o_data_w = ;
     o_overflow_w = ;
     o_valid_w = ;
+    overflow_result = ;
+    case(i_inst)
+        3'b000: begin // ADD
+
+            // overflow logic
+            overflow_result = i_data_a + i_data_b;
+            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+                o_overflow_w = 1'b1;
+            end else begin
+                o_overflow_w = 1'b0;     
+            end
+
+            // arithmetic logic
+            o_data_w = i_data_a + i_data_b;
+        end
+
+        3'b001: begin // SUBTRACT
+
+            // overflow logic
+            overflow_result = i_data_a - i_data_b;
+            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+                o_overflow_w = 1'b1;
+            end else begin
+                o_overflow_w = 1'b0;     
+            end
+
+            // arithmetic logic
+            o_data_w = i_data_a - i_data_b;
+        end
+
+        3'b010: begin // MULTIPLY 
+
+            // overflow logic
+            overflow_result = ((i_data_a * i_data_b) + 5'd16) >>> 5; // Adding 2 ^ (5-1) and then truncate 5 LSBs for rounding fraction
+            
+            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+                o_overflow_w = 1'b1;
+            end else begin
+                o_overflow_w = 1'b0;     
+            end
+
+            // arithmetic logic
+            o_data_w = overflow_result;
+        end
+
+        3'b011: begin // MAC
+
+            // arithmetic logic
+            o_data_w = i_data_a * i_data_b;
+
+            // overflow logic
+            overflow_result = i_data_a * i_data_b;
+            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+                o_overflow_w = 1'b1;
+            end else begin
+                o_overflow_w = 1'b0;     
+            end
+        end
+
+    endcase
 end
 
 

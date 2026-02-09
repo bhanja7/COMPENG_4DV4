882cc654a660296d0f9251a8eba1ca712dec8684
ALI BHANJI <bhanja7@mcmaster.ca>
2026-02-09 01:55:13 -0500

    logic for all operations added

--- MiniProject_01/1101_hw1/alu.v
+++ MiniProject_01/1101_hw1/alu.v
@@ -17,8 +17,9 @@ reg  [11:0] o_data_w, o_data_r;
 reg         o_valid_w, o_valid_r;
 reg         o_overflow_w, o_overflow_r;
 // ---- Add your own wires and registers here if needed ---- //
-reg signed [23:0] overflow_result; // wire
-ref signed [11:0] accumulator; // register
+reg signed [23:0] result_long; // wire
+reg signed [11:0] accumulator; // register
+reg signed [23:0] sum_long; // wire
 
 
 
@@ -41,13 +42,14 @@ always@(*) begin
     o_data_w = ;
     o_overflow_w = ;
     o_valid_w = ;
-    overflow_result = ;
+    result_long = ;
+    sum_long = ;
     case(i_inst)
         3'b000: begin // ADD
 
             // overflow logic
-            overflow_result = i_data_a + i_data_b;
-            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+            result_long = i_data_a + i_data_b;
+            if(result_long[23:12] != {12{result_long[11]}}) begin
                 o_overflow_w = 1'b1;
             end else begin
                 o_overflow_w = 1'b0;     
@@ -60,8 +62,8 @@ always@(*) begin
         3'b001: begin // SUBTRACT
 
             // overflow logic
-            overflow_result = i_data_a - i_data_b;
-            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+            result_long = i_data_a - i_data_b;
+            if(result_long[23:12] != {12{result_long[11]}}) begin
                 o_overflow_w = 1'b1;
             end else begin
                 o_overflow_w = 1'b0;     
@@ -74,32 +76,64 @@ always@(*) begin
         3'b010: begin // MULTIPLY 
 
             // overflow logic
-            overflow_result = ((i_data_a * i_data_b) + 5'd16) >>> 5; // Adding 2 ^ (5-1) and then truncate 5 LSBs for rounding fraction
+            result_long = ((i_data_a * i_data_b) + 5'd16) >>> 5; // Adding 2 ^ (5-1) and then truncate 5 LSBs for rounding fraction
             
-            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+            if(result_long[23:12] != {12{result_long[11]}}) begin
                 o_overflow_w = 1'b1;
             end else begin
                 o_overflow_w = 1'b0;     
             end
 
             // arithmetic logic
-            o_data_w = overflow_result;
+            o_data_w = result_long[11:0];
         end
 
         3'b011: begin // MAC
 
-            // arithmetic logic
-            o_data_w = i_data_a * i_data_b;
+            // MAC logic
+            result_long = ((i_data_a * i_data_b) + 5'd16) >>> 5; // Adding 2 ^ (5-1) and then truncate 5 LSBs for rounding fraction
+            sum_long = result_long + accumulator;
 
             // overflow logic
-            overflow_result = i_data_a * i_data_b;
-            if(overflow_result[23:12] != {12{overflow_result[11]}}) begin
+            if(result_long[23:12] != {12{result_long[11]}} || sum_long[23:12] != {12{sum_long[11]}}) begin
                 o_overflow_w = 1'b1;
             end else begin
                 o_overflow_w = 1'b0;     
             end
+
+            // arithmetic logic
+            o_data_w = sum_long[11:0];
         end
 
+        3'b100: begin // BITWISE XNOR
+
+            o_data_w = ~(i_data_a ^ i_data_b);
+        
+        end
+
+        3'b101: begin // ReLU
+            if(i_data_a[11]) begin
+                o_data_w = 12'b0;
+            end else begin
+                o_data_w = i_data_a;
+            end
+        end
+
+        3'b110: begin // Mean
+            
+            o_data_w = (i_data_a + i_data_b) >>> 2;
+
+        end
+
+        3'b111: begin // Absolute Max
+
+            if(i_data_b[10:0] > i_data_a[10:0] ) begin
+                o_data_w = i_data_b;
+            end else begin
+                o_data_w = i_data_a;
+            end        
+
+        end
     endcase
 end
 
@@ -120,6 +154,12 @@ always@(posedge i_clk or negedge i_rst_n) begin
         o_data_r <= o_data_w;
         o_overflow_r <= o_overflow_w;
         o_valid_r <= o_valid_w;
+
+        if(i_inst == 3'b011) begin
+            accumulator <= sum_long;            
+        end else begin
+            accumulator <= 12'b0;
+        end
     end
 end
 
